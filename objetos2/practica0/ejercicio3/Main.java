03ecb058a08c50b9727c694483f91940e88f364b
incaminatoleonardo <incaminatoleonardo@gmail.com>
2022-03-13 18:00:00 -0300

    clase anemica-V0.2

--- objetos2/practica0/ejercicio3/Main.java
+++ objetos2/practica0/ejercicio3/Main.java
@@ -1,11 +1,25 @@
 package ejercicio3;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("No anemico?");
 		Tiempo.ImprimirfechaFormatoLargo();
 		Tiempo.ImprimirfechaFormatoCorto();
+
+		TiempoAnemica hora = new TiempoAnemica();
+
+		System.out.println("Anemico?");
+		hora.setHoraCorta(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).format(LocalDate.now()));
+		System.out.println(hora.getHoraCorta());
+		hora.setHoraLarga(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(LocalDate.now()));
+		System.out.println(hora.getHoraLarga());
+
 	}
 
 }
---
+++ objetos2/practica0/ejercicio3/TiempoAnemica.java
@@ -0,0 +1,24 @@
+package ejercicio3;
+
+public class TiempoAnemica {
+
+	private String horaCorta;
+	private String horaLarga;
+
+	public String getHoraCorta() {
+		return horaCorta;
+	}
+
+	public void setHoraCorta(String string) {
+		this.horaCorta = string;
+	}
+
+	public String getHoraLarga() {
+		return horaLarga;
+	}
+
+	public void setHoraLarga(String horaLarga) {
+		this.horaLarga = horaLarga;
+	}
+
+}
